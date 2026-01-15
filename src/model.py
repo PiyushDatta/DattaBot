fix up gradient checkpointing for xla/tpu part 2

src/model.py
@@ -28,6 +28,9 @@ from torch import (
 )
 from torch.nn.attention import sdpa_kernel, SDPBackend
 
+def _is_gradient_checkpointing_enabled(device: torch_device) -> bool:
+    config = get_agent_config()
+    return config.neural_net.gradient_checkpointing and device.type != "xla" and not device.type.startswith("xla")
 
 # Transformer Model.
 class DattaBotModel(nn.Module):
@@ -55,10 +58,7 @@ class DattaBotModel(nn.Module):
             n_layers=self.n_layers, d_model=self.d_model, device=self.device
         )
         self.final_norm = RMSNorm(dim=self.d_model)
-        self.gradient_checkpointing = (
-            self.config.neural_net.gradient_checkpointing
-            and device.type != "xla" and not device.type.startswith("xla")
-        )
+        self.gradient_checkpointing = _is_gradient_checkpointing_enabled()
         if self.gradient_checkpointing:
             self._enable_gradient_checkpointing()
 
@@ -173,7 +173,7 @@ class TransformerDecoderBlock(nn.Module):
         # Feed forward.
         self.position_wise_ffn = TransformerPositionWiseFeedForward()
         self.position_wise_ffn_norm = RMSNorm(dim=embedded_dim_size)
-        self.gradient_checkpointing = self.config.neural_net.gradient_checkpointing
+        self.gradient_checkpointing = _is_gradient_checkpointing_enabled()
 
     def forward(
         self,